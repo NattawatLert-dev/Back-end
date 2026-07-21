460824627d31293b39d359dbbe561299030509e1
Nattawat Lertchidumrongsri <NattawatLert0747@gmail.com>
2026-07-21 19:32:02 +0700

    feat: add Aritmetic

---
+++ PHP/1 - Basics/3 - Arithmetic.php
@@ -0,0 +1,34 @@
+<?php
+    // Arithmetic operator
+    // + - * / ** %
+
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    //$z = $x + $y;
+    //$z = $x - $y;
+    //$z = $x * $y;
+    //$z = $x / $y;
+    //$z = $x ** $y;
+    //$z = $z % $y; 
+    echo $z;
+
+    // Increment/decrement operator
+
+    $counter = 0;
+
+    //$counter++;
+    //$counter--
+    //$counter+=2;
+    //$counter-=2;
+    echo $counter;
+
+    // Oprator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+
+    $total = 1 + 2 - 3 * 4 / 5 ** 6;
+?> 
